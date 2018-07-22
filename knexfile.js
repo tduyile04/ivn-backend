7b303d9480167573d7ae478962a3a45f4f398c94
Inumidun Amao <inumidun.amao@andela.com>
2018-07-22 22:38:14 +0100

    use heroku env vars

--- knexfile.js
+++ knexfile.js
@@ -6,7 +6,8 @@ module.exports = {
     connection: {
       database: process.env.DB_NAME,
       user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST
     },
     pool: {
       min: 2,
